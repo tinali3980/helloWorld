c922065e04e7d9bc7bdfe2a1728057e28de4b009
tinali3980 <tli12@terpmail.umd.edu>
2024-01-31 21:12:16 -0500

    Adding first HTML page with hello world message

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Tina Li!'
-
-
+    return 'Hello World from Tina Li! This is my first code change.'
 if __name__ == '__main__':
     app.run()
+
+@app.route('/hello')
+def hello():
+    return render_template('index.html')
