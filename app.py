db5f0f2af85ef0a78f95962de992cb8464d8fc6a
tinali3980 <tli12@terpmail.umd.edu>
2024-02-15 01:04:47 -0500

    This is my app.py file.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Tina Li! This is my first code change.'
+    return 'Hello World from Tina Li! This is my first code change'
 
 @app.route('/hello')
 def hello():
