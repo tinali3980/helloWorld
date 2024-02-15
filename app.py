2c67c253efe9d98fcf6703ed5c644a9dbb1c0d32
tinali3980 <tli12@terpmail.umd.edu>
2024-02-14 21:14:51 -0500

    Adding about HTML page with information about myself.

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Tina Li! This is my first code change.'
-if __name__ == '__main__':
-    app.run()
 
 @app.route('/hello')
 def hello():
-    return render_template('index.html')
+    return render_template('hello.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run()
