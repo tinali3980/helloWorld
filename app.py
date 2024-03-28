4119d90f129c11b176d3755c79b12cca13e307f6
tinali3980 <tli12@terpmail.umd.edu>
2024-03-28 19:09:26 -0400

    This is my updated file for the assignment containing base, contact, and favorite course html.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,27 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    message = "You entered your favorite course as: {} {}".format(subject, course_number)
+    print(message)
+
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html',form_submitted=True)
+    else:
+        return render_template('contact.html')
+    return render_template('contact.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
